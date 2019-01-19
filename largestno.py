8ea692d4f3b6e32b6eebd64cb7b771d10f114f85
princyalex <41040391+princyalex@users.noreply.github.com>
2019-01-19 14:46:50 +0530

    Create largestno.py

---
+++ largestno.py
@@ -0,0 +1,11 @@
+n1 =1 
+n2 = 2
+n3 = 3
+if (n1 >= n2) and (n1 >= n3):
+   largest = n1
+elif (n2 >= n1) and (n2 >= n3):
+   largest = n2
+else:
+   largest = n3
+
+print("The largest number between",n1,",",n2,"and",n3,"is",largest)
